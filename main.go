52d96d5f8aed994e3b420c63f5f50843b39bf5cc
hsmtkk <hsmtkk@gmail.com>
2023-10-26 10:20:32 +0900

    output ot GITHUB_OUTPUT

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Println("begin")
+
 	fmt.Println("output environment variables")
 	for _, kv := range os.Environ() {
 		fmt.Println(kv)
 	}
+
+	fmt.Println("output to GITHUB_OUTPUT")
+	gitHubOutput := os.Getenv("GITHUB_OUTPUT")
+	fooInput := os.Getenv("FOO_INPUT")
+
+	output(gitHubOutput, fooInput)
+
 	fmt.Println("end")
+}
+
+func output(gitHubOutput, fooInput string) {
+	f, err := os.Open(gitHubOutput)
+	if err != nil {
+		log.Fatalf("failed to open file %s: %v", gitHubOutput, err)
+	}
+	defer f.Close()
+
+	s := fmt.Sprintf("Your input was %s, is it right?", fooInput)
+	f.WriteString(s)
 }
